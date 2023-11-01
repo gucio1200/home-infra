e3594cbc7d594112d581c208d493885002988c3e
casper.gogol <casper.gogol@kyotutechnology.com>
2023-11-01 05:35:55 +0100

    Refactor

--- k8s/apps/home-automation/node-red/app/config/settings.js
+++ k8s/apps/home-automation/node-red/app/config/settings.js
@@ -1,34 +1,6 @@
 module.exports = {
   flowFile: "flows.json",
-  credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
   flowFilePretty: true,
-
-  adminAuth: {
-    type: "strategy",
-    strategy: {
-      name: "openidconnect",
-      autoLogin: true,
-      label: "Sign in",
-      icon: "fa-cloud",
-      strategy: require("passport-openidconnect").Strategy,
-      options: {
-        issuer: "https://authentik.bjw-s.dev/application/o/node-red/",
-        authorizationURL:
-          "https://authentik.bjw-s.dev/application/o/authorize/",
-        tokenURL: "https://authentik.bjw-s.dev/application/o/token/",
-        userInfoURL: "https://authentik.bjw-s.dev/application/o/userinfo/",
-        clientID: "hN1VIEAhdR3noLaM6KLmKBYRWVsf5ywyFfh98GDV",
-        clientSecret: process.env.NODE_RED_OAUTH_CLIENT_SECRET,
-        callbackURL: "https://nodered.bjw-s.dev/auth/strategy/callback",
-        scope: ["email", "profile", "openid"],
-        proxy: true,
-        verify: function (issuer, profile, done) {
-          done(null, profile)
-        },
-      },
-    },
-    users: [{username: "bernd", permissions: ["*"]}],
-  },
 
   uiPort: process.env.PORT || 1880,
 
