b07e854b0e713d088b56197e3147c39be06d6594
casper.gogol <casper.gogol@kyotutechnology.com>
2023-11-01 05:27:59 +0100

    Add node-red

---
+++ k8s/apps/home-automation/node-red/app/config/settings.js
@@ -0,0 +1,86 @@
+module.exports = {
+  flowFile: "flows.json",
+  credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
+  flowFilePretty: true,
+
+  adminAuth: {
+    type: "strategy",
+    strategy: {
+      name: "openidconnect",
+      autoLogin: true,
+      label: "Sign in",
+      icon: "fa-cloud",
+      strategy: require("passport-openidconnect").Strategy,
+      options: {
+        issuer: "https://authentik.bjw-s.dev/application/o/node-red/",
+        authorizationURL:
+          "https://authentik.bjw-s.dev/application/o/authorize/",
+        tokenURL: "https://authentik.bjw-s.dev/application/o/token/",
+        userInfoURL: "https://authentik.bjw-s.dev/application/o/userinfo/",
+        clientID: "hN1VIEAhdR3noLaM6KLmKBYRWVsf5ywyFfh98GDV",
+        clientSecret: process.env.NODE_RED_OAUTH_CLIENT_SECRET,
+        callbackURL: "https://nodered.bjw-s.dev/auth/strategy/callback",
+        scope: ["email", "profile", "openid"],
+        proxy: true,
+        verify: function (issuer, profile, done) {
+          done(null, profile)
+        },
+      },
+    },
+    users: [{username: "bernd", permissions: ["*"]}],
+  },
+
+  uiPort: process.env.PORT || 1880,
+
+  diagnostics: {
+    enabled: true,
+    ui: true,
+  },
+
+  runtimeState: {
+    enabled: false,
+    ui: false,
+  },
+
+  logging: {
+    console: {
+      level: "info",
+      metrics: false,
+      audit: false,
+    },
+  },
+
+  contextStorage: {
+    default: {
+      module: "localfilesystem",
+    },
+  },
+
+  exportGlobalContextKeys: false,
+
+  externalModules: {},
+
+  editorTheme: {
+    tours: false,
+
+    projects: {
+      enabled: false,
+      workflow: {
+        mode: "manual",
+      },
+    },
+
+    codeEditor: {
+      lib: "monaco",
+      options: {},
+    },
+  },
+
+  functionExternalModules: true,
+  functionGlobalContext: {},
+
+  debugMaxLength: 1000,
+
+  mqttReconnectTime: 15000,
+  serialReconnectTime: 15000,
+}
